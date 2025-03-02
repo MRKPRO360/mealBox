f6210a96540cc7fe7ea4c94cc1df220c6e52f99f
Rezaul <mdrezaulkarrim@gmail.com>
2025-03-02 10:28:10 +0600

    adde recipe interface and model

---
+++ src/app/modules/recipe/recipe.interface.ts
@@ -0,0 +1,38 @@
+export interface IIngredient {
+  name: string;
+  quantity: string;
+  contains?: string[];
+}
+
+export interface INutritionValues {
+  calories: string;
+  fat: string;
+  saturated_fat: string;
+  carbohydrate: string;
+  sugar: string;
+  dietaryFiber: string;
+  protein: string;
+  cholesterol: string;
+  sodium: string;
+}
+
+export interface IInstruction {
+  step: number;
+  description: string;
+}
+
+export interface IRecipe {
+  recipeMenuName: string;
+  recipeImage: string;
+  recipeName: string;
+  description: string;
+  tags: string[];
+  allergens: string[];
+  totalTime: string;
+  prepTime: string;
+  difficulty: string;
+  ingredients: IIngredient[];
+  nutritionValues: INutritionValues;
+  utensils: string[];
+  instructions: IInstruction[];
+}
---
+++ src/app/modules/recipe/recipe.model.ts
@@ -0,0 +1,58 @@
+import { model, Schema } from 'mongoose';
+import {
+  IIngredient,
+  IInstruction,
+  INutritionValues,
+  IRecipe,
+} from './recipe.interface';
+
+// Define the Ingredient schema
+const ingredientSchema = new Schema<IIngredient>({
+  name: { type: String, required: true },
+  quantity: { type: String, required: true },
+  contains: { type: [String], default: [] }, // Optional field for allergens
+});
+
+// Define the NutritionValues schema
+const nutritionValuesSchema = new Schema<INutritionValues>({
+  calories: { type: String, required: true },
+  fat: { type: String, required: true },
+  saturated_fat: { type: String, required: true },
+  carbohydrate: { type: String, required: true },
+  sugar: { type: String, required: true },
+  dietaryFiber: { type: String, required: true },
+  protein: { type: String, required: true },
+  cholesterol: { type: String, required: true },
+  sodium: { type: String, required: true },
+});
+// Define the Instruction schema
+const instructionSchema = new Schema<IInstruction>({
+  step: { type: Number, required: true },
+  description: { type: String, required: true },
+});
+
+// Define the Recipe schema
+const recipeSchema = new Schema<IRecipe>(
+  {
+    recipeName: { type: String, required: true },
+    recipeMenuName: { type: String, required: true },
+    description: { type: String, required: true },
+    tags: { type: [String], required: true },
+    allergens: { type: [String], required: true },
+    totalTime: { type: String, required: true },
+    prepTime: { type: String, required: true },
+    difficulty: { type: String, required: true },
+    ingredients: { type: [ingredientSchema], required: true },
+    nutritionValues: { type: nutritionValuesSchema, required: true },
+    utensils: { type: [String], required: true },
+    instructions: { type: [instructionSchema], required: true },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// Create the Recipe model
+const Recipe = model<IRecipe>('Recipe', recipeSchema);
+
+module.exports = Recipe;
