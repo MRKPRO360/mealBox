a925c72b447f7ea6f2e8fa00836f4e8c888ff488
Rezaul <mdrezaulkarrim@gmail.com>
2025-04-24 04:54:42 +0600

    Remove dist folder from repo and add to .gitignore, updated showing plan for future

--- dist/app/interface/order.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ORDER_STATUS = void 0;
-exports.ORDER_STATUS = {
-    PENDING: 'pending',
-    INPROGRESS: 'inProgress',
-    DELIVERED: 'delivered',
-    CANCELED: 'canceled',
-};
